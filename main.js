30691824152c3c086ec8b9d24340aeb5b902c4ad
Felipe C Arreola <Felipe.arreola12@hotmail.com>
2018-11-04 17:55:19 -0800

    Update main.js
    Added a drop down  for navigation bar.

--- main.js
+++ main.js
@@ -3,3 +3,18 @@ $(updateView)
 function updateView() {
     alert("Using jQuery!")
 }
+
+/* Drop Down Function-Nav Bar */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+  }
+}
