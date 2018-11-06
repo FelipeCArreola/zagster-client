5c674ef437fcff559cd9bdc3813f7f6c3a06c420
Felipe C Arreola <Felipe.arreola12@hotmail.com>
2018-11-05 18:13:44 -0800

    Minor Update main.js
    Adding "//" to close my comment

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown if the user clicks outside of it-----**Source**:https://www.w3schools.com/css/css_dropdowns.asp"
+// Close the dropdown if the user clicks outside of it-----**Source**:https://www.w3schools.com/css/css_dropdowns.asp" //
 window.onclick = function(e) {
   if (!e.target.matches('.dropbtn')) {
     var myDropdown = document.getElementById("myDropdown");
