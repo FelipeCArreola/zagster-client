cff9b62c03bad4db28a7154bc05a58768dfe19f2
Felipe C Arreola <Felipe.arreola12@hotmail.com>
2018-11-05 18:07:53 -0800

    Update main.js
    Updating main.js: Removing Alert; Giving source credit of JS being utilized that supports my Drop Down button; adding "$.getJSON" to acquire count from "https://zagster-service.herokuapp.com"; and updating "<h2>" to display ".html(numberOfRides)" = total amount of rides.

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+const BASE_URL = "https://zagster-service.herokuapp.com"
+
 $(updateView)
 
 function updateView() {
-    alert("Welcome, Nyaaa!")
+    $.getJSON(BASE_URL + "/rides/count" , updateRideCount)  
+}
+
+function updateRideCount(data) {
+    numberOfRides = data.count 
+    $("h2#rideCount").html(numberOfRides)
 }
 
 /* Drop Down Function-Nav Bar */
@@ -9,7 +16,7 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown if the user clicks outside of it
+// Close the dropdown if the user clicks outside of it-----**Source**:https://www.w3schools.com/css/css_dropdowns.asp"
 window.onclick = function(e) {
   if (!e.target.matches('.dropbtn')) {
     var myDropdown = document.getElementById("myDropdown");
