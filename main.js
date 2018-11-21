6ac42ad86ec48d5f1ecc5af1ab1057f13dff2595
Felipe C Arreola <Felipe.arreola12@hotmail.com>
2018-11-21 13:38:03 -0800

    Update main.js
    Updating JS, putting const.

--- main.js
+++ main.js
@@ -1,15 +1,50 @@
 const BASE_URL = "https://zagster-service.herokuapp.com"
 
+
+const G5_TC = "/rides/count/g5"
+const COL_TC = "/rides/count/columbia_simpson"
+const DRAKE_TC = "/rides/count/drake_park"
+const GALV_TC = "/rides/count/galveston"
+const GRC_TC = "/rides/count/grc"
+const OLD_TC = "/rides/count/old_mill"
+const OSU_TC = "/rides/count/osu_cascades"
+const TEN_TC = "/rides/count/ten_barrel"
+
+
+
 $(updateView)
 
 function updateView() {
-    $.getJSON(BASE_URL + "/rides/count" , updateRideCount)  
+  
+    $.getJSON(BASE_URL + G5_TC, updateRideCount)
+    $.getJSON(BASE_URL + G5_TC, printData)
 }
 
 function updateRideCount(data) {
     numberOfRides = data.count 
     $("h2#rideCount").html(numberOfRides)
 }
+
+function printData(data) {
+    console.log(data)
+}
+
+
+
+ 
+
+
+
+
+/* Visualization JS- Source:"https://www.chartjs.org/docs/latest/charts/bar.html" */
+
+
+
+            
+  
+
+        
+
 
 /* Drop Down Function-Nav Bar  -----**Source**:https://www.w3schools.com/css/css_dropdowns.asp" */
 function myFunction() {
