ed39cefb9bed3f208cd07a0efbeab6d33506e6ba
Felipe C Arreola <Felipe.arreola12@hotmail.com>
2018-11-26 13:24:41 -0800

    Update main.js
    moving script of chart from index.html to main.js. Adding console logs of data strings

--- main.js
+++ main.js
@@ -11,39 +11,155 @@ const OSU_TC = "/rides/count/osu_cascades"
 const TEN_TC = "/rides/count/ten_barrel"
 
 
+var ChartData = []
+
 
 $(updateView)
 
 function updateView() {
   
-    $.getJSON(BASE_URL + G5_TC, updateRideCount)
-    $.getJSON(BASE_URL + G5_TC, printData)
+    $.getJSON(BASE_URL + G5_TC, updateChartData00)
+    $.getJSON(BASE_URL + COL_TC, updateChartData01)
+    $.getJSON(BASE_URL + DRAKE_TC, updateChartData02)
+    $.getJSON(BASE_URL + GALV_TC, updateChartData03)
+    $.getJSON(BASE_URL + GRC_TC, updateChartData04)
+    $.getJSON(BASE_URL + OLD_TC, updateChartData05)
+    $.getJSON(BASE_URL + OSU_TC, updateChartData06)
+    $.getJSON(BASE_URL + TEN_TC, updateChartData07)
 }
 
-function updateRideCount(data) {
-    numberOfRides = data.count 
-    $("h2#rideCount").html(numberOfRides)
+function updateChartData00(data) {
+    ChartData.push(data.count)
 }
 
-function printData(data) {
-    console.log(data)
+function updateChartData01(data) {
+    ChartData.push(data.count)
 }
 
+function updateChartData02(data) {
+    ChartData.push(data.count)
+}
 
+function updateChartData03(data) {
+    ChartData.push(data.count)
+}
 
- 
+function updateChartData04(data) {
+    ChartData.push(data.count)
+}
 
+function updateChartData05(data) {
+    ChartData.push(data.count)
+}
 
+function updateChartData06(data) {
+    ChartData.push(data.count)
+}
 
+function updateChartData07(data) {
+    ChartData.push(data.count)
+}
+
+/* Console log Strings */
+function updateChartData00(data) {
+	console.log(data)
+}
+function updateChartData01(data) {
+	console.log(data)
+}
+function updateChartData02(data) {
+	console.log(data)
+}
+function updateChartData03(data) {
+	console.log(data)
+}
+function updateChartData04(data) {
+	console.log(data)
+}
+function updateChartData05(data) {
+	console.log(data)
+}
+function updateChartData06(data) {
+	console.log(data)
+}
+function updateChartData07(data) {
+	console.log(data)
+}
 
 /* Visualization JS- Source:"https://www.chartjs.org/docs/latest/charts/bar.html" */
 
 
+window.onload = function() {
+var ctx = document.getElementById("myChart").getContext('2d')
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["G5", "Columbia/Simpson", "Drake Park", "Galveston", "GRC", "Old Mill", "OSU-Cascades", "Ten Barrel"],
+        datasets: [{
+            label: 'Total Count of Rides Per Station',
+            data: ChartData,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(58,112,42,0.2)',
+                'rgba(76,212,40,0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(54, 162, 217, 1)',
+                'rgba(76,242,40,1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+      
+      legend: {
+            labels: {
+                fontColor: 'white',
+                fontSize: 15.5
+            }
+        },
 
-            
+      scales: {
+            xAxes: [{gridLines: {
+                      display: false ,
+                      color: 'rgba(255,255,255, 0.8)'
+                   },
+                    ticks: {
+                      fontColor: 'white',
+                      fontSize: 14
+                    }
+            }]  ,
+
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true,
+                    fontColor: 'white',
+                    fontSize: 14
+                },
+                gridLines: {
+                      display: false ,
+                      color: 'rgba(255,255,255, 0.8)'
+                },  
+            }]
+
+        }
+    }
   
+});
 
-        
+}
+
 
 
 /* Drop Down Function-Nav Bar  -----**Source**:https://www.w3schools.com/css/css_dropdowns.asp" */
